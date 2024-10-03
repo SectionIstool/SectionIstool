80a071edebb3012ade1120887c66ae8c9ad28947
黎泽懿 <lzy.12@foxmail.com>
2024-10-03 13:43:44 +0800

    更新

--- SectionIstool/Menus.py
+++ SectionIstool/Menus.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PyQt5.QtWidgets import QMenu, QAction
 
 
@@ -17,24 +16,4 @@ def createMenus(self):
     # 点击选项时
     check_update_action.triggered.connect(lambda: self.update_version())
     releases.addAction(check_update_action)
-=======
-from PyQt5.QtWidgets import QMenu, QAction
-
-
-# 导入版本信息
-from config import version_info
-# 在 createMenus 函数中
-current_version = version_info()
-
-
-# 创建菜单
-def createMenus(self):
-    # 创建文件菜单，并显示当前版本
-    releases = QMenu(f'当前版本: {current_version}', self)
-    # 再文件菜单下添加检查更新的选项
-    check_update_action = QAction('检查更新', self)
-    # 点击选项时
-    check_update_action.triggered.connect(lambda: self.update_version())
-    releases.addAction(check_update_action)
->>>>>>> 4fa2a7888d33a4299e04b4d0deacd959883e7656
     self.menuBar().addMenu(releases)
--- SectionIstool/error_handle.py
+++ SectionIstool/error_handle.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PyQt5.QtWidgets import QMessageBox, QApplication
 import os
 import shutil
@@ -27,33 +26,3 @@ def error_handle(self):
 
     # 关闭应用程序
     QApplication.instance().quit()  # 使用 PyQt 的 quit 来关闭应用程序
-=======
-from PyQt5.QtWidgets import QMessageBox, QApplication
-import os
-import shutil
-
-
-# 程序报错
-def error_handle(self):
-    try:
-        # 定义所有需要删除的文件和文件夹路径
-        paths_to_delete = [
-            os.path.join('Config', 'encs'),
-            os.path.join('Config', 'keys'),
-            # os.path.join('Downloads')
-        ]
-        
-        # 遍历路径列表，删除存在的文件或文件夹
-        for path in paths_to_delete:
-            if os.path.exists(path):
-                if os.path.isdir(path):
-                    shutil.rmtree(path)
-                else:
-                    os.remove(path)
-    except Exception:
-        # 错误处理机制，错误信息
-        self.show_custom_message(self, "错误", f"删除文件或文件夹时出错\n请手动删除以下文件或文件夹:\n{paths_to_delete}", QMessageBox.Warning)
-
-    # 关闭应用程序
-    QApplication.instance().quit()  # 使用 PyQt 的 quit 来关闭应用程序
->>>>>>> 4fa2a7888d33a4299e04b4d0deacd959883e7656
